074602e3db3c6682c1581154a26d35e781016c93
rmajanss <rmajanss.janssens@ugent.be>
2019-10-10 15:42:33 +0200

    Action and State

--- 3_deliberative/resin-janssens-ex3/src/datatypes/Action.java
+++ 3_deliberative/resin-janssens-ex3/src/datatypes/Action.java
@@ -1,5 +1,31 @@
 package datatypes;
 
-public class Action {
+import logist.topology.Topology.City;
 
+public class Action {
+	public enum ActionType {
+		MOVE,
+		DELIVER,
+		PICKUP
+	}
+	
+	private ActionType type;
+	private City destinationCity;
+	
+	public Action(ActionType type){
+		this.type = type;
+	}
+	
+	public Action(ActionType type, City city){
+		this.type = type;
+		this.destinationCity = city;
+	}
+	
+	public ActionType getType(){
+		return type;
+	}
+	
+	public City getDestinationCity(){
+		return destinationCity;
+	}
 }
--- 3_deliberative/resin-janssens-ex3/src/datatypes/State.java
+++ 3_deliberative/resin-janssens-ex3/src/datatypes/State.java
@@ -1,5 +1,35 @@
 package datatypes;
 
-public class State {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import logist.topology.Topology.City;
+
+public class State {
+	private City currentCity;
+	private Map<City, Map<City, Integer>> cityMap;
+	
+	public State(List<City> cities){
+		cityMap = new HashMap<City, Map<City, Integer>>();
+		for(City city : cities){
+			cityMap.put(city, new HashMap<City, Integer>());
+			for(City city2 : cities){
+				cityMap.get(city).put(city2, 0);
+			}
+		}
+	}
+	
+	public Map<City, Map<City, Integer>> getCityMap(){
+		return cityMap;
+	}
+	
+	public City getCurrentCity(){
+		return currentCity;
+	}
+	
+	public void setCurrentCity(City city){
+		currentCity = city;
+	}
+	
 }
